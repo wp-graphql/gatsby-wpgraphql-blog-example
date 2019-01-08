60ec2bb6822dd372fa28b61bff721002b48b23df
Jason Bahl <jasonbahl@mac.com>
2019-01-07 19:06:07 -0700

    - Update .gitignore to ignore .idea files - add ant design and gatsby-source-graphql - configure ant-design and gatsby-source-graphql (to pull from WPGraphQL.com)

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,20 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    // Include Ant Design component library.
+    `gatsby-plugin-antd`,
+    // Setup WPGraphQL.com to be the source
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // This type will contain remote schema Query type
+        typeName: "WPGraphQL",
+        // This is field under which it's accessible
+        fieldName: "wpgraphql",
+        // Url to query from
+        url: "https://www.wpgraphql.com/graphql",
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
